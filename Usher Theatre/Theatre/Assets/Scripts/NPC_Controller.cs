959c617f060169dac40dbfcf79c83f8a36056db9
Slurp1e <jamesb.devpen@gmail.com>
2019-11-01 15:17:10 +0000

    NPC Finalized Version (0.1.3f)

---
+++ Usher Theatre/Theatre/Assets/Scripts/NPC_Controller.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPC_Controller : MonoBehaviour
+{
+    public bool inAction = false;
+
+    public float randomActionTick = 0f;
+    public float randomMovementTick = 0f;
+    public float actionTicks = 0f;
+    public float movementTicks = 0f;
+
+    public List<int> probabilities = new List<int>();
+    public int RubbishDropRange = 5;
+    public int QuestionRange = 4;
+    public int ArgumentRange = 1;
+    
+    NavMeshAgent agent;
+
+    //Action List
+    // 1 - Rubbish Drop
+    // 2 - Question
+    // 3 - Argument
+
+    public float getRandomFloat(float range1, float range2){
+        return Random.Range(range1, range2);;
+    }
+
+    public Vector3 getRandomPosition(){
+       return new Vector3(getRandomFloat(-3f, 25.25f), getRandomFloat(0f, 0f), getRandomFloat(3f, -40f));
+    }
+
+    void Start(){
+        agent = GetComponent<NavMeshAgent>();
+
+        randomMovementTick = Random.Range(5f, 10f);
+        movementTicks = 0f;
+
+        randomActionTick = Random.Range(20f, 30f);
+        actionTicks = 0f;
+
+        for (int i = 0; i < RubbishDropRange; i++){
+            probabilities.Add(1);
+        }
+        for (int i = 0; i < QuestionRange; i++){
+            probabilities.Add(2);
+        }
+        for (int i = 0; i < ArgumentRange; i++){
+            probabilities.Add(3);
+        }
+    }
+    
+
+    void Update()
+    {
+        movementTicks += Time.deltaTime;
+        actionTicks += Time.deltaTime;
+        if (actionTicks >= randomActionTick){
+            actionTicks = 0f;
+            randomActionTick = Random.Range(20f, 30f);
+            int i = Random.Range(0, probabilities.Count);
+
+            switch (probabilities[i])
+            {
+                case 1:
+                    Debug.Log("Drop Rubbish");
+                    break;
+                case 2:
+                    Debug.Log("Question");
+                    break;
+                case 3:
+                    Debug.Log("Argument");
+                    break;
+            }
+        }
+
+        if (movementTicks > randomMovementTick){
+            movementTicks = 0f;
+            randomMovementTick = Random.Range(5f, 10f);
+
+            agent.SetDestination(getRandomPosition());
+        }
+    }
+}
--- Usher Theatre/Theatre/Assets/Scripts/Spawn_NPC.cs
+++ Usher Theatre/Theatre/Assets/Scripts/Spawn_NPC.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPC_Controller : MonoBehaviour
+public class Spawn_NPC : MonoBehaviour
 {
+    public GameObject NPC;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 10; i++){
+            Instantiate(NPC, transform.position, Quaternion.identity);
+        }   
     }
 
     // Update is called once per frame
