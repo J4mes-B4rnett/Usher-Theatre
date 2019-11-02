8c32f0a60d15d931c09a6c3ccc486b0590e4e21c
Slurp1e <jamesb.devpen@gmail.com>
2019-11-02 07:38:18 +0000

    Revert "Revert "Each Unity project file has been moved to the master branch""
    This reverts commit a4fd9e83ccd12f5cd07a55e2bcb20773ec63167d.

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player_Controller : MonoBehaviour
+{
+    public Camera cam; 
+    NavMeshAgent agent;
+    public GameObject ring;
+
+    public bool playerSelected = false;
+
+    void Start(){
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update(){
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)){
+                if (playerSelected == false){
+                    if (hit.collider.tag == "Floor"){
+                        Instantiate(ring, hit.point, Quaternion.identity);
+                        agent.SetDestination(hit.point);
+                    } 
+
+                    if (hit.collider.tag == "Player"){
+                        playerSelected = true;
+                    } 
+                    Debug.Log("Move to Floor");
+                }
+
+                if (playerSelected){
+                    if (hit.collider.tag == "Rubbish"){
+                        agent.SetDestination(hit.transform.position);
+                        Debug.Log("Move to Rubbish");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/destroyMe.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyMe : MonoBehaviour
+{
+
+    public float timeSinceStart = 0f;
+
+    void Update()
+    {
+        timeSinceStart += Time.deltaTime;
+        if (timeSinceStart >= 1f){
+            Destroy(this.gameObject);
+        }
+    }
+}
