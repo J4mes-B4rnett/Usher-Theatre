a0cd22784e502c599dea9ef845ca65c70bdc8056
Cody <Creepercatsgaming@gmail.com>
2019-11-05 14:32:37 -0500

    Wants, State Machine, Patrons

---
+++ Assets/Scripts/Patrons/Wants/Wants.cs
@@ -0,0 +1,16 @@
+﻿[System.Serializable]
+public abstract class Wants
+{
+     public string want;
+     public int wantAmount;
+     public int wantsUntilAction;
+
+     public Wants(string want, int wantAmount, int wantsUntilAction)
+     {
+          this.want = want;
+          this.wantAmount = wantAmount;
+          this.wantsUntilAction = wantsUntilAction;
+     }
+
+     public abstract void ExecuteWant(Patron patron);
+}
